08b5d5893cab04cfcfec41f15c3710f36c3995c7
Administrator <Administrator@2023-7094>
2025-03-13 09:05:59 +0900

    DataStructure_1_5880807

--- DataStructure_1_5880807/0311.c
+++ DataStructure_1_5880807/0311.c
@@ -3,10 +3,10 @@
 int main() {
 	unsigned int n;
 	printf("정수를 입력하세요: ");
-	scanf_s("%d", &n);
+	scanf_s("%d", &n);//정수 입력
 	printf("입력된 정수의 32비트 표현: ");
 	for (int i = 31; i >= 0; --i) {
 		int m = n >> i & 1;
 		printf("%d", m);
-	}
+	}//입력된 정수를 32비트로 표현하는 과정
 }
